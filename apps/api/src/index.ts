6ae9ff303846555c817e05cd757d973acfb2e3d3
David Stafa <david.stafa@gmail.com>
2026-01-20 22:20:31 +0100

    feat: initial implementation of better-auth in api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,27 @@
-import { BASE_URL, PORT } from "./config.js"
+import { BASE_URL, PORT } from './config.js'
 import express from 'express'
+import { auth, fromNodeHeaders, toNodeHandler } from '@habbit-tracker/auth'
+import cors from 'cors'
 
 const app = express()
+
+app.all('/api/auth/*splat', toNodeHandler(auth))
+
+// Configure CORS middleware
+app.use(
+  cors({
+    origin: 'http://your-frontend-domain.com', // Replace with your frontend's origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+)
+
+app.get('/api/me', async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  })
+  return res.json(session)
+})
 
 app.get('/', (req, res) => {
   res.send(`Hello World! ${BASE_URL}`)
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,10 +1,10 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "@habbit-tracker/db";
+import { betterAuth } from 'better-auth'
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+import { prisma } from '@habbit-tracker/db'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql",
+    provider: 'postgresql',
   }),
   emailAndPassword: {
     enabled: true,
@@ -16,7 +16,10 @@ export const auth = betterAuth({
   //     clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
   //   },
   // },
-});
+})
 
 // Export types for use in client and server
-export type Session = typeof auth.$Infer.Session;
+export type Session = typeof auth.$Infer.Session
+
+// TODO: Check with Béďos if this is the correct way to export the helper
+export { toNodeHandler, fromNodeHeaders } from 'better-auth/node'
